2bdb7d8d9caebd1aad5b5b4e144ee2d3a245411a
Imelda <imeldamendi@gmail.com>
2022-09-08 13:50:04 -0700

    5th

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints out the string
+ * with proper grammar,but the outcome is a piece of art,
+ * Return: This function returns 0
+ */
+Int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+}
