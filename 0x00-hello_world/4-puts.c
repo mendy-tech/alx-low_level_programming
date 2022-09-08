2320d75135411d1fcaad797ee43b82383b8f7865
Imelda <imeldamendi@gmail.com>
2022-09-08 14:26:24 -0700

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - This function prints out the string
+ * "Programming is like building a multilingual puzzle
+ * Return: Function returns 0
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
