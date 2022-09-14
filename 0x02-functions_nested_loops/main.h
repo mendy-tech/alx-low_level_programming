a39960a3acc1b160988fc7c0c9af3fb469684401
Imelda <imeldamendi@gmail.com>
2022-09-14 10:40:18 -0700

    first

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,13 @@
-#include <stdio.h>
-
-/**
- * _putchar - Print out text
- * Return: void
- */
-
-void _putchar(char s)
-{
-	putchar(s);
-}
-/**
- * main-outputs _putchar
- * Return: zero
- */
+void _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
