6f93ac14392fc5ff234ac7329b2227fafc300fad
Imelda <imeldamendi@gmail.com>
2022-10-07 03:43:35 -0700

    hundred

---
+++ 0x0C-more_malloc_free/100-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - checks the code.
+ *
+ * Return:Always 0.
+ */
+int main(void)
+{
+	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+	return (0);
+}
