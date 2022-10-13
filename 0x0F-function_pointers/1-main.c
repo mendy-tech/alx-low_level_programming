80fa7bdefb6ff718fde68730df14c79854f2d5f7
Imelda <imeldamendi@gmail.com>
2022-10-13 02:50:56 -0700

    second

---
+++ 0x0F-function_pointers/1-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- checks the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void array_iterator(int *array, size_t size, void (*action)(int));
+	return (0);
+}
