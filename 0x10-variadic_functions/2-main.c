c8f4e9765701616c2781396abac76a61da6e6128
Imelda <imeldamendi@gmail.com>
2022-10-14 11:46:16 -0700

    third

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - checks the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void print_strings(const char *separator, const unsigned int n, ...);
+	return (0);
+}
